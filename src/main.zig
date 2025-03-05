bddc85e84b0bb526152139957beef78b48203367
Zablas <kar.balciunas@gmail.com>
2025-03-06 01:11:56 +0200

    Implement request header parsing

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,12 @@ const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
     const socket = try SocketConf.Socket.init();
-    try stdout.print("Server Addr: {any}\n", .{socket._address});
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
 
     var buffer = [_]u8{0} ** 1000;
+
     try Request.read_request(connection, buffer[0..buffer.len]);
-    try stdout.print("{s}\n", .{buffer});
+    const request = Request.parse_request(buffer[0..buffer.len]);
+    try stdout.print("{any}\n", .{request});
 }
